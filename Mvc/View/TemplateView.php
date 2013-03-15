27cbc0b5dc9005c4388c949755c56e66f911445e
joyqi <magike.net@gmail.com>
2013-03-15 17:35:54 +0800

    fix view

--- Mvc/View/TemplateView.php
+++ Mvc/View/TemplateView.php
@@ -79,18 +79,28 @@ class TemplateView extends AbstractView
     {
         global $template;
 
-        $file = $this->_template;
-        $data = $this->vars;
-        $prefix = $this->_prefix;
+        $_file = $this->_template;
+        $_data = $this->vars;
+        $_prefix = $this->_prefix;
 
-        $template = function ($file) use ($data, $prefix) {
+        $template = function ($_file) use ($_data, $_prefix) {
             global $template;
 
-            extract($data);
-            require $prefix . $file;
+            extract($_data);
+            $_files = is_array($_file) ? $_file : array($_file);
+
+            foreach ($_files as $_file) {
+                $_file = $_prefix . $_file;
+                if (file_exists($_file)) {
+                    require $_file;
+                    return;
+                }
+            }
+
+            throw new \Exception('Template file not found');
         };
 
-        $template($file);
+        $template($_file);
     }
 }
 
