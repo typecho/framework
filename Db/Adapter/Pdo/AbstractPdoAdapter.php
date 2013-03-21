152abbb4fe8b0621826f4f450457c7c14dd42387
joyqi <magike.net@gmail.com>
2013-03-21 09:27:26 +0800

    fix pdo exception

--- Db/Adapter/Pdo/AbstractPdoAdapter.php
+++ Db/Adapter/Pdo/AbstractPdoAdapter.php
@@ -37,10 +37,9 @@ abstract class AbstractPdoAdapter extends AbstractAdapter
      */
     public function __construct($dsn, $user = NULL, $password = NULL, array $options = array())
     {
-        \PDO::setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
         try {
             $this->_pdo = new \PDO($dsn, $user, $password, array_merge(array(
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_TIMEOUT => 10
             ), $options));
         } catch (\PDOException $e) {
