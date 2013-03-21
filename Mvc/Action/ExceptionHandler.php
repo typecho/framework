6d61e440d99152efd457700654f38bf9431ef1fd
joyqi <magike.net@gmail.com>
2013-03-21 14:24:17 +0800

    fix excepiton handler

--- Mvc/Action/ExceptionHandler.php
+++ Mvc/Action/ExceptionHandler.php
@@ -44,45 +44,7 @@ class ExceptionHandler extends AbstractAction
      */
     public function execute()
     {
-        $lines = array_filter(explode("\n", $this->_exception->getTraceAsString()));
-        $result = array();
-        foreach ($lines as $line) {
-            $line = trim($line);
-
-            if (strpos($line, ':')) {
-                list ($prefix, $func) = explode(':', $line);
-                list ($num, $file) = explode(' ', $prefix, 2);
-                $func = str_replace(array('&lt;?php&nbsp;&nbsp;', "\n"), '', highlight_string('<?php ' . $func, true));
-
-                $result[] = "{$num} <cite>$file</cite> {$func}";
-            } else {
-                $result[] = $line;
-            }
-        }
-
-        $h = '<p style="background: #fee; border: 1px solid #660; color: #600; padding: .5em"><strong>' . get_class($this->_exception) . '</strong>: [' 
-            . $this->_exception->getCode() . '] ' . $this->_exception->getMessage() . '</p>';
-
-        $trace = '<pre style="background: #fff; border: 1px solid #000; padding: 0 .5em; overflow: auto; display: block"><p>' 
-            . implode('</p><p>', $result) . '</p></pre>';
-
-        $body = '<html><head><title>Exception</title><style>code {background: #e1e1e1; border: 1px solid #000; padding: .1em; 
-            overflow: auto} cite { font-style: normal; font-weight: bold; font-size: .9em }</style></head>'
-            . '<body style="background: #eeeeff; font-family: verdana, arial, helvetica, sans-serif">' . $h . $trace . '</body></html>';
-
-        return array('error', $this->_content ?: $body, $this->_template);
-    }
-
-    /**
-     * setContent
-     * 
-     * @param mixed $content
-     * @access public
-     * @return void
-     */
-    public function setContent($content)
-    {
-        $this->_content = $content;
+        return array('error', $this->_exception, $this->_template);
     }
 
     /**
