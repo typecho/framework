d231d0c0f170e9204970396b71adb39aedfd068a
joyqi <magike.net@gmail.com>
2013-05-08 12:06:49 +0800

    fix db define

--- Mvc/Service/Db/AbstractTable.php
+++ Mvc/Service/Db/AbstractTable.php
@@ -3,6 +3,7 @@
 namespace TE\Mvc\Service\Db;
 
 use TE\Mvc\Base;
+use TE\Db\Connector;
 
 /**
  * Class AbstractTable
@@ -12,25 +13,87 @@ use TE\Mvc\Base;
 abstract class AbstractTable extends Base
 {
     /**
+     * db  
+     * 
+     * @var mixed
+     */
+    private $db;
+
+    /**
+     * _table  
+     * 
+     * @var string
+     */
+    private $_table;
+
+    /**
+     * _primaryKey  
+     * 
+     * @var string
+     */
+    private $_primaryKey;
+
+    /**
+     * setDb  
+     * 
+     * @param Connector $db 
+     * @access public
+     * @return void
+     */
+    public function setDb(Connector $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
      * 获取Db对象
      *
      * @return \TE\Db\Connector
      */
-    abstract public function getDb();
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+    /**
+     * setTable  
+     * 
+     * @param string $table 
+     */
+    public function setTable($table)
+    {
+        return $this->_table = $table;
+    }
 
     /**
      * 获取表名
      *
      * @return string
      */
-    abstract public function getTable();
+    public function getTable()
+    {
+        return $this->_table;
+    }
+
+    /**
+     * setPrimaryKey  
+     * 
+     * @param string $primaryKey 
+     */
+    public function setPrimaryKey($primaryKey)
+    {
+        $this->_primaryKey = $primaryKey;
+    }
 
     /**
      * 获取主键名
      *
      * @return string
      */
-    abstract public function getPrimaryKey();
+    public function getPrimaryKey()
+    {
+        return $this->_primaryKey;
+    }
 
     /**
      * add
